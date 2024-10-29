d45f0c8fa016e1c3ddb5a60a2e6d6c235446bfb4
shajalal07s <khanranu81@gmail.com>
2024-10-29 14:16:39 +0600

    Hero Section crate and tailwind config file customize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#A53DFF',
+        'secendary': '#EDD8FF80',
+        'Text-color': '#556070',
+        'foot1-color': '#132238',
+        'footer': '#2B384C'
+      },
+      fontFamily: {
+        'work-sans': 'Work Sans'
+      },
+      width: {
+        'primary-w': '58px',
+        '90%': '90%',
+        'hero-i-w': '536px'
+      },
+      height: {
+        'primary-h': '58px',
+        'hero-i-h': '636px'
+      }
+    },
   },
   plugins: [],
 }
